a2041a37009d9eedc13dcded12c789f2f1f16fd3
jishnusen <jishnu1@gmail.com>
2019-08-09 16:40:52 -0700

    clean up bcsax and add gauss gui

--- bcsax.py
+++ bcsax.py
@@ -1,43 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import fortran_90_ports.bcsaxfort as fort
 import pandas as pd
-
-
-def run():
-    print("Running Non-Axial CSA Averaging Simulation")
-    w0 = float(input("Enter larmor frequency (MHz): "))
-    s11 = float(input("Enter sigma11 (ppm): "))
-    s22 = float(input("Enter sigma22 (ppm): "))
-    s33 = float(input("Enter sigma33 (ppm): "))
-    jfreq = float(input("Enter jump frequency (Hz): "))
-    t2 = float(input("Enter T2 (seconds): "))
-    hi = float(input("Enter upper plot limit: "))
-    lo = float(input("Enter lower plot limit: "))
-    
-    f,g = fort.bcsax(s11,s22,s33,jfreq,t2,hi,lo,w0)
-
-    gen_data = plt.plot(f,g,label="Simulation") 
-
-    data = True if input("Plot against own data (y/n): ") == "y" else False
-
-    if data:
-        infile = np.loadtxt(input("Filename: "), delimiter=",")
-        user_data = plt.plot(infile[:, 0], infile[:, 1], label="User Data")
-
-    plt.legend(loc='upper right')
-
-    plt.gca().invert_xaxis()
-    plt.show()
-
-    save = True if input("Save (y/n): ") == "y" else False
-
-    if save:
-        out = open(input("Filename: "), "w")
-        csv = ""
-        for i in range(0, g.size):
-            csv += f"{float(f[i])},{float(g[i])}\n"
-        out.write(csv)
 
 def plot(w0,s11,s22,s33,jfreq,t2,hi,lo, user_data):
     global f,g
--- gauss.py
+++ gauss.py
@@ -1,17 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def run():
-    print("Running Gaussian Peak Exchange Simulation")
-    vlf = float(input("Larmor Frequency (MHz): "))
-    pos = np.fromstring(input("Enter peak positions (ppm, comma separated): "), sep=",") 
-    inp = np.fromstring(input("Enter intensities (comma separated): "), sep=",")
-    lo = float(input("Upper plot limit: "))
-    hi = float(input("Lower plot limit: "))
-    t2 = float(input("T2 (seconds): ")) 
-    jfreq = float(input("Jump Frequency (Hz): "))
-
-    pos = pos / vlf
+def plot(vlf, pos, inp, lo, hi, t2, jfreq, user_data):
+    global f,g
     vlf = vlf * 2. * np.pi
     inp = inp.T
     w = pos * vlf
@@ -45,10 +36,8 @@ def run():
 
     gen_data = plt.plot(f,g,label="Simulation") 
 
-    data = True if input("Plot against own data (y/n): ") == "y" else False
-
-    if data:
-        infile = np.loadtxt(input("Filename: "), delimiter=",")
+    if user_data != "":
+        infile = np.loadtxt((user_data), delimiter=",")
         user_data = plt.plot(infile[:, 0], infile[:, 1], label="User Data")
 
     plt.legend(loc='upper right')
@@ -56,11 +45,69 @@ def run():
     plt.gca().invert_xaxis()
     plt.show()
 
-    save = True if input("Save (y/n): ") == "y" else False
 
-    if save:
-        out = open(input("Filename: "), "w")
-        csv = ""
-        for i in range(0, g.size):
-            csv += f"{float(f[i])},{float(g[i])}\n"
-        out.write(csv)
+import tkinter as tk
+from tkinter import filedialog
+
+
+
+master = tk.Tk()
+
+tk.Label(master, text="larmor frequency (MHz)").grid(row=0)
+tk.Label(master, text="peak pos (csv, ppm)").grid(row=1)
+tk.Label(master, text="intensities (csv, rel)").grid(row=2)
+tk.Label(master, text="lower plot limit (ppm)").grid(row=3)
+tk.Label(master, text="upper plot limit (ppm)").grid(row=4)
+tk.Label(master, text="T2 (seconds)").grid(row=5)
+tk.Label(master, text="jump frequency (Hz)").grid(row=6)
+v = tk.StringVar()
+user_data = ""
+tk.Label(master, textvariable=v).grid(row=7, column = 1)
+
+def callback():
+    global user_data
+    plot(float(e_vlf.get()), np.fromstring(e_pos.get(), sep=","), np.fromstring(e_inp.get(), sep=","), float(e_lo.get()), 
+        float(e_hi.get()), float(e_t2.get()), float(e_jfreq.get()), user_data)
+
+def filechoose():
+    global user_data
+    user_data = filedialog.askopenfilename()
+    v.set(user_data.rsplit('/', 1)[-1])
+
+def save():
+    global f,g
+    csv = ""
+    out = filedialog.asksaveasfile(mode='w')
+    for i in range(0, g.size):
+        csv += f"{float(f[i])},{float(g[i])}\n"
+    out.write(csv)
+    out.close()
+
+e_vlf = tk.Entry(master)
+e_pos = tk.Entry(master)
+e_inp = tk.Entry(master)
+e_lo = tk.Entry(master)
+e_hi = tk.Entry(master)
+e_t2 = tk.Entry(master)
+e_jfreq = tk.Entry(master)
+
+e_vlf.grid(row=0, column=1)
+e_pos.grid(row=1, column=1)
+e_inp.grid(row=2, column=1)
+e_lo.grid(row=3, column=1)
+e_hi.grid(row=4, column=1)
+e_t2.grid(row=5, column=1)
+e_jfreq.grid(row=6, column=1)
+
+tk.Button(master, text="BROWSE", command = filechoose).grid(row=7)
+tk.Button(master, text="PLOT", command = callback).grid(row=8)
+tk.Button(master, text="SAVE", command = save).grid(row=8, column = 1)
+
+def main():
+    global master
+    master.title("gauss")
+    master.mainloop()
+
+if __name__ == "__main__":
+    main()
+
